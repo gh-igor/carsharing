c486638b987a6fcd4f95a73335f5868070301bb6
Igor White <for-lance@mail.ru>
2021-08-06 14:59:03 +0300

    Fix SearchForm layout

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,27 +1,32 @@
 import { TextField } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 import styles from './SearchForm.module.css'
 
 export default function SearchForm({ values: { km, min }, onChange }) {
   return (
-    <>
-      <TextField
-        name='km'
-        value={km}
-        onChange={onChange}
-        type='number'
-        className={styles.field}
-        label="Расстояние, км"
-        variant="outlined"
-      />
-      <TextField
-        name='min'
-        value={min}
-        onChange={onChange}
-        type='number'
-        className={styles.field}
-        label="Время, мин"
-        variant="outlined"
-      />
-    </>
+    <Grid container>
+      <Grid item xs={12} sm={6} className={styles.left}>
+        <TextField
+          name='km'
+          value={km}
+          onChange={onChange}
+          type='number'
+          className={styles.field}
+          label="Расстояние, км"
+          variant="outlined"
+        />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <TextField
+          name='min'
+          value={min}
+          onChange={onChange}
+          type='number'
+          className={styles.field}
+          label="Время, мин"
+          variant="outlined"
+        />
+      </Grid>
+    </Grid>
   )
 }
